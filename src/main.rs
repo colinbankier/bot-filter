0d6c356fc52e6544ed564e57497b155247e13b8b
colinbankier <colinbankier@gmail.com>
2017-11-03 23:11:57 +1000

    Parse json events.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 extern crate cidr;
-use cidr::{Ipv4Cidr, Cidr, NetworkParseError};
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
+
+use cidr::{Ipv4Cidr, Cidr};
 use std::net::Ipv4Addr;
 use std::str::FromStr;
 use std::{env, process};
@@ -7,12 +13,19 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::io;
 use std::io::BufReader;
-use std::error::Error;
+use serde_json::Error;
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 struct Event {
-    source_ip: Ipv4Addr,
+    source_ip: String,
     session_id: String
+}
+
+impl FromStr for Event {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        serde_json::from_str(s)
+    }
 }
 
 struct Args {
@@ -33,8 +46,6 @@ impl Args {
 }
 
 fn main() {
-    println!("Hello, world!");
-
     let cmd_args: Vec<String> = env::args().collect();
     let args = Args::new(&cmd_args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
@@ -42,13 +53,38 @@ fn main() {
     });
 
     let cidrs = cidrs_list(&args.cidr_filename);
-    let events = get_events(&args.events_filename);
-    let filtered = events.iter().filter(|event|
-        cidrs.iter().any(|cidr| cidr.contains(&event.source_ip))
-    );
-    for event in filtered {
-        println!("{}", event.session_id);
+    let file = File::open(&args.events_filename).expect("file not found");
+    let mut buf_reader = BufReader::new(file);
+    for line in buf_reader.lines() {
+        match line {
+            Ok(event_str) => {
+                match Event::from_str(&event_str) {
+                    Ok(event) => {
+                        match event.source_ip.parse() {
+                            Ok(ip) => {
+                                if cidrs.iter().any(|cidr| cidr.contains(&ip)) {
+                                    println!("{}", event.session_id);
+                                }
+                            },
+                            Err(err) => {
+                                eprintln!("Invalid ip address {}", event.source_ip);
+                            }
+                        }
+                    },
+                    Err(err) => {
+                        eprintln!("Invalid event {}", err);
+                    }
+                }
+            },
+            Err(err) => eprintln!("Error reading line {}", err)
+        }
     }
+    // let filtered = events.iter().filter(|event|
+    //     cidrs.iter().any(|cidr| cidr.contains(&event.source_ip))
+    // );
+    // for event in filtered {
+    //     println!("{}", event.session_id);
+    // }
 }
 
 fn cidrs_list(filename: &str) -> Vec<Ipv4Cidr> {
@@ -72,11 +108,13 @@ fn read_file_lines(filename: &str) -> io::Result<Vec<String>> {
     let mut file = File::open(filename).expect("file not found");
     let mut buf_reader = BufReader::new(file);
     buf_reader.lines().collect()
-    // let mut contents = String::new();
-    // file.read_to_string(&mut contents)
-    //     .expect("something went wrong reading the file");
-    // contents
 }
+
+// fn line_reader(filename: &str) -> BufReader<String> {
+//     let mut file = File::open(filename).expect("file not found");
+//     let mut buf_reader = BufReader::new(file);
+//     buf_reader
+// }
 
 fn get_events(filename: &str) -> Vec<Event> {
     vec![
