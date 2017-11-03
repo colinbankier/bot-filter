51d1282bda062c5201cea31d26fff597141a503d
colinbankier <colinbankier@gmail.com>
2017-11-03 21:59:02 +1000

    failed partition.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 extern crate cidr;
-use cidr::{Ipv4Cidr, Cidr};
+use cidr::{Ipv4Cidr, Cidr, NetworkParseError};
 use std::net::Ipv4Addr;
 use std::str::FromStr;
 use std::{env, process};
 use std::io::prelude::*;
 use std::fs::File;
+use std::io;
 use std::io::BufReader;
+use std::error::Error;
 
 #[derive(Debug)]
 struct Event {
@@ -35,7 +37,7 @@ fn main() {
 
     let cmd_args: Vec<String> = env::args().collect();
     let args = Args::new(&cmd_args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -50,21 +52,31 @@ fn main() {
 }
 
 fn cidrs_list(filename: &str) -> Vec<Ipv4Cidr> {
-    vec![
-        Ipv4Cidr::from_str("10.1.0.0/16").unwrap(),
-        Ipv4Cidr::from_str("10.2.0.0/16").unwrap()
-    ];
-    let cidrs = read_file(filename)
-
-
+    // vec![
+    //     Ipv4Cidr::from_str("10.1.0.0/16").unwrap(),
+    //     Ipv4Cidr::from_str("10.2.0.0/16").unwrap()
+    // ];
+    let cidrs = read_file_lines(filename).unwrap_or_else(|err| {
+        eprintln!("Problem reading cidrs: {}", err);
+        process::exit(1);
+    });
+    let (oks, fails): (Result<Ipv4Cidr, NetworkParseError>, Result<Ipv4Cidr, NetworkParseError>) = cidrs.iter().map(|line| {
+        Ipv4Cidr::from_str(line)
+    }).partition(Result::is_ok);
+    for fail in fails {
+        eprintln!("Invalid cidr {}", fail);
+    }
+    oks.iter().filter_map(|cidr| cidr.unwrap_or(None)).collect::<Vec<Ipv4Cidr>>()
 }
 
-fn read_file_lines(filename: &str) -> String {
+fn read_file_lines(filename: &str) -> io::Result<Vec<String>> {
     let mut file = File::open(filename).expect("file not found");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-    contents
+    let mut buf_reader = BufReader::new(file);
+    buf_reader.lines().collect()
+    // let mut contents = String::new();
+    // file.read_to_string(&mut contents)
+    //     .expect("something went wrong reading the file");
+    // contents
 }
 
 fn get_events(filename: &str) -> Vec<Event> {
